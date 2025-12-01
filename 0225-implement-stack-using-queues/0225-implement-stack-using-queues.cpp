0fe05bcf8db5394cde9fb9a1ebba6c8a6758ce18
prashant072@ <thakurprashant1223@gmail.com>
2025-12-01 16:10:24 +0530

    Time: 0 ms (100%), Space: 9.4 MB (54.36%) - LeetHub

--- 0225-implement-stack-using-queues/0225-implement-stack-using-queues.cpp
+++ 0225-implement-stack-using-queues/0225-implement-stack-using-queues.cpp
@@ -1,24 +1,45 @@
-class MyStack {
+class Node {
 public:
-    list<int> st;  
+    int data;
+    Node* next;
 
-    MyStack() {}
-
-    void push(int x) {
-        st.push_front(x); 
-    }
-
-    int pop() {
-        int val = st.front(); 
-        st.pop_front();      
-        return val;
-    }
-
-    int top() {
-        return st.front();   
-    }
-
-    bool empty() {
-        return st.empty();
+    Node(int val) {
+       data = val;
+       next = nullptr;
     }
 };
+
+class MyStack {
+    Node* head = nullptr;
+public:
+    void push(int x) {
+        Node* n = new Node(x);
+        n->next = head;
+        head = n;
+    }
+    
+    int pop() {
+        int v = head->data;
+        Node* temp = head;
+        head = head->next;
+        delete temp;
+        return v;
+    }
+    
+    int top() {
+        return head->data;
+    }
+    
+    bool empty() {
+         return head == nullptr;
+    }
+};
+
+/**
+ * Your MyStack object will be instantiated and called as such:
+ * MyStack* obj = new MyStack();
+ * obj->push(x);
+ * int param_2 = obj->pop();
+ * int param_3 = obj->top();
+ * bool param_4 = obj->empty();
+ */
