9a50b62c1d6c2b7482639767382517adfa231fe0
prashant072@ <thakurprashant1223@gmail.com>
2025-12-14 16:10:57 +0530

    Time: 0 ms (100.00%) | Memory: 16.3 MB (24.25%) - LeetSync

---
+++ 404-sum-of-left-leaves/sum-of-left-leaves.cpp
@@ -0,0 +1,30 @@
+class Solution {
+public:
+    int sumOfLeftLeaves(TreeNode* root) {
+        if (!root) return 0;
+
+        int sum = 0;
+        stack<TreeNode*> st;
+        st.push(root);
+
+        while (!st.empty()) {
+            TreeNode* node = st.top();
+            st.pop();
+
+            if (node->left) {
+            
+                if (!node->left->left && !node->left->right) {
+                    sum += node->left->val;
+                } else {
+                    st.push(node->left);
+                }
+            }
+
+            if (node->right) {
+                st.push(node->right);
+            }
+        }
+
+        return sum;
+    }
+};
