0dd25ebdbb2b964e4fa627d3f7b479f1a4a299d1
prashant072@ <thakurprashant1223@gmail.com>
2026-05-24 10:33:47 +0530

    Time: 4 ms (34.38%) | Memory: 18.6 MB (26.94%) - LeetSync

---
+++ 347-top-k-frequent-elements/top-k-frequent-elements.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    vector<int> topKFrequent(vector<int>& nums, int k) {
+        sort(nums.begin(), nums.end());
+        vector<pair<int, int>> v; 
+        int n = nums.size();
+        int count = 1;
+        for (int i = 1; i < n; i++) {
+            if (nums[i] == nums[i - 1]) {
+                count++;
+            } else {
+                v.push_back({count, nums[i - 1]});
+                count = 1;
+            }
+        }
+        v.push_back({count, nums[n - 1]});
+        sort(v.rbegin(), v.rend());
+        vector<int> ans;
+        for (int i = 0; i < k; i++) {
+            ans.push_back(v[i].second);
+        }
+        return ans;
+    }
+};
