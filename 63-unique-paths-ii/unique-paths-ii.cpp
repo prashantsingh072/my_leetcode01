e70e8452066489f89d265c835b6e54d9fb39dfee
prashant072@ <thakurprashant1223@gmail.com>
2026-09-03 11:15:07 +0530

    Time: 3 ms (9.58%) | Memory: 12 MB (31.57%) - LeetSync

--- 63-unique-paths-ii/unique-paths-ii.cpp
+++ 63-unique-paths-ii/unique-paths-ii.cpp
@@ -1,3 +1,28 @@
+// class Solution {
+// public:
+//     int solve(int i, int j, vector<vector<int>>& a, vector<vector<int>>& dp) {
+//         int m = a.size(), n = a[0].size();
+
+//         if (i >= m || j >= n || a[i][j] == 1)
+//             return 0;
+
+//         if (i == m - 1 && j == n - 1)
+//             return 1;
+
+//         if (dp[i][j] != -1)
+//             return dp[i][j];
+
+//         return dp[i][j] = solve(i + 1, j, a, dp) +
+//                           solve(i, j + 1, a, dp);
+//     }
+
+//     int uniquePathsWithObstacles(vector<vector<int>>& a) {
+//         int m = a.size(), n = a[0].size();
+//         vector<vector<int>> dp(m, vector<int>(n, -1));
+
+//         return solve(0, 0, a, dp);
+//     }
+// };
 class Solution {
 public:
     int solve(int i, int j, vector<vector<int>>& a, vector<vector<int>>& dp) {
@@ -12,8 +37,10 @@ public:
         if (dp[i][j] != -1)
             return dp[i][j];
 
-        return dp[i][j] = solve(i + 1, j, a, dp) +
-                          solve(i, j + 1, a, dp);
+        dp[i][j] = solve(i + 1, j, a, dp) +
+                   solve(i, j + 1, a, dp);
+
+        return dp[i][j];
     }
 
     int uniquePathsWithObstacles(vector<vector<int>>& a) {
